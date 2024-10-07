e9a2028f4055d023f59e169a87421be17a803277
DannyCroDev <danielrajic26@outlook.com>
2024-10-07 17:35:09 +0200

    code for more models, functions for relations and query functions

--- app/Models/PlayerAchievement.php
+++ app/Models/PlayerAchievement.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class PlayerAchievement extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'player_id',
+        'achievement_id',
+        'date_earned'
+    ];
+
+    public function playerAchievements()
+    {
+        return $this->belongsToMany(Achievement::class, 'player_achievements');
+    }
 }
