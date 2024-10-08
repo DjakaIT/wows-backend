035c33a33f51c55e71fe2eba7f72489040512939
DannyCroDev <danielrajic26@outlook.com>
2024-10-08 15:22:23 +0200

    code for clan  model and fillables, defining database table relationships

--- app/Models/PlayerShip.php
+++ app/Models/PlayerShip.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class PlayerShip extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+
+        'player_id',
+        'ship_id',
+        'battles_count',
+        'wins_count',
+        'damage_dealt',
+        'average_damage',
+        'frags',
+        'survival_rate'
+    ];
+
+
+    public function player()
+    {
+        return $this->belongsTo(Player::class);
+    }
+
+    public function ship()
+    {
+        return $this->belongsTo(Ship::class);
+    }
 }
--- app/Models/Ship.php
+++ app/Models/Ship.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Ship extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+
+        'ship_id',
+        'name',
+        'tier',
+        'type',
+        'nation'
+    ];
+
+
+    public function player()
+    {
+        return $this->hasMany(PlayerShip::class);
+    }
+
+    public function Battle()
+    {
+        return $this->hasMany(BattleParticipant::class);
+    }
 }
