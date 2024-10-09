86e51e7601b07dc1f7312e37c394eb051c503c79
DannyCroDev <danielrajic26@outlook.com>
2024-10-09 15:24:06 +0200

    ship controller methods updated

--- app/Http/Controllers/ShipController.php
+++ app/Http/Controllers/ShipController.php
@@ -2,9 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ship;
 use Illuminate\Http\Request;
 
 class ShipController extends Controller
 {
-    //
+    //display all ships
+
+    public function index()
+    {
+        $ships = Ship::all();
+        return response()->json($ships);
+    }
+
+    //display particular ships
+    public function show($id)
+    {
+        $ships = Ship::findOrFail($id);
+        return response()->json($ships);
+    }
+
+    //save a ship
+    public function store(Request $request)
+    {
+
+        $validatedShipData = $request->validate([
+            'name' => 'required|string|max:150',
+            'tier' => 'required|integer',
+            'type' => 'required|integer',
+            'nation' => 'required|string|max:80',
+            'ship_id' => 'required|unique:ships, ship_id'
+        ]);
+
+        $ship = Ship::create($validatedShipData);
+        return response()->json($ship, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $ship = Ship::findOrFail($id);
+
+        $validatedUpdatedShipData = $request->validate([
+            'name' => 'required|string|max:150',
+            'tier' => 'required|integer',
+            'type' => 'required|integer',
+            'nation' => 'required|string|max:80',
+            'ship_id' => 'required|unique:ships, ship_id'
+        ]);
+
+
+        $ship->update($validatedUpdatedShipData);
+        return response()->json($ship);
+    }
+
+    //delete a ship
+
+    public function destroy($id)
+    {
+        $ship = Ship::findOrFail($id);
+        $ship->delete();
+
+        return response()->json(['message' => 'Ship deleted succesfully from records']);
+    }
 }
