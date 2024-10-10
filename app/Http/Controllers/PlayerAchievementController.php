57c78c7c53046993c48dce3faa2c9ab2e8aeee4e
DannyCroDev <danielrajic26@outlook.com>
2024-10-10 19:02:16 +0200

    added other routes

--- app/Http/Controllers/PlayerAchievementController.php
+++ app/Http/Controllers/PlayerAchievementController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Player;
 use Illuminate\Http\Request;
+
 
 class PlayerAchievementController extends Controller
 {
-    //
+    public function index() {}
 }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,30 @@ Route::prefix('players')->group(function () {
     Route::put('/{id}', [PlayerController::class, 'update']);
     Route::delete('/{id}', [PlayerController::class, 'destroy']);
 });
+
+Route::prefix('ships')->group(function () {
+    Route::get('/', [ShipController::class, 'index']);
+    Route::get('/{id}', [ShipController::class, 'show']);
+    Route::post('/', [ShipController::class, 'store']);
+    Route::put('/{id}', [ShipController::class, 'update']);
+    Route::delete('/{id}', [ShipController::class, 'destroy']);
+});
+
+
+Route::prefix('clans')->group(function () {
+    Route::get('/', [ClanController::class, 'index']);
+    Route::get('/{id}', [ClanController::class, 'show']);
+    Route::post('/', [ClanController::class, 'store']);
+    Route::put('/{id}', [ClanController::class, 'update']);
+    Route::delete('/{id}', [ClanController::class, 'destroy']);
+});
+
+
+
+Route::prefix('battles')->group(function () {
+    Route::get('/', [BattleController::class, 'index']);
+    Route::get('/{id}', [BattleController::class, 'show']);
+    Route::post('/', [BattleController::class, 'store']);
+    Route::put('/{id}', [BattleController::class, 'update']);
+    Route::delete('/{id}', [BattleController::class, 'destroy']);
+});
