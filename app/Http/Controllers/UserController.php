103f9fb1f2a5998429944db036624303dee92dfe
DannyCroDev <danielrajic26@outlook.com>
2024-10-11 11:49:53 +0200

    login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+    }
 }
