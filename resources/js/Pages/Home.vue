c8c658bb336a638300cb3d339bd887a3934473d1
zlajaa1 <vujicic.zlatko@gmail.com>
2024-10-22 17:50:19 +0200

    passing data from back to front trough inertia

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,3 +1,10 @@
 <template>
-    <div>Homepage</div>
+    <div>Homepage - {{ user }}</div>
 </template>
+
+<script setup>
+    // Laravel passing data trough Inertia, recieving trough props in VUE
+    defineProps({
+        user: String,
+    });
+</script>
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return Inertia::render('Home');
+    return Inertia::render('Home', [
+        'user' => 'Kralj Tomislav'
+    ]);
 });
