8fd2633d6ff7b98863e36a2f2779119dd1fd2f65
DannyCroDev <danielrajic26@outlook.com>
2024-11-13 16:53:32 +0100

    created necessary files to fetch player stat data, code added soon, boilerplate

--- app/Services/PlayerStatisticService.php
+++ app/Services/PlayerStatisticService.php
@@ -20,6 +20,9 @@ class PlayerStatisticService
     public function fetchAndStorePlayerStats()
     {
         try {
+            //retrieve players by account id from database and count the total
+            $playerIds = Player::pluck('account_id')->all();
+            Log::info('Data loadeDd', ['player_count' => count($playerIds)]);
         } catch (\Exception $e) {
             Log::error("Error fetching achievements" . $e->getMessage());
         }
